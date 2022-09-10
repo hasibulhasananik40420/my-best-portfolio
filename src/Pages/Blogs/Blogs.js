d2f358850602ebc899058e16dade1c6851573243
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-09-10 12:25:22 +0600

    added some blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -12,42 +12,42 @@ const Blogs = () => {
 
 
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mt-12 pb-12 '>
-                    <div className='relative md:w-[400px] w-full font-san'>
+                    <div className=' md:w-[400px] w-full font-san'>
                         <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-1.jpg" alt="" />
-                        <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[144px]'>11 Dec, 22</p>
+                        {/* <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[144px]'>11 Dec, 22</p> */}
                         <h1 className='text-2xl font-semibold text-white mt-2'>Top tools for Photographers</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
                     </div>
 
-                    <div className='relative md:w-[400px] w-full font-san'>
+                    <div className=' md:w-[400px] w-full font-san'>
                         <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-2.jpg" alt="" />
-                        <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[144px]'>12 Aug, 22</p>
+                        {/* <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[144px]'>12 Aug, 22</p> */}
                         <h1 className='text-2xl font-semibold text-white mt-2'>Take a tour of my office</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
                     </div>
 
-                    <div className='relative md:w-[400px] w-full font-san'>
+                    <div className=' md:w-[400px] w-full font-san'>
                         <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-3.jpg" alt="" />
-                        <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[176px]'>4 Feb, 22</p>
+                        {/* <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[120px] bottom-[176px]'>4 Feb, 22</p> */}
                         <h1 className='text-2xl font-semibold text-white mt-2'>How i became a Web Designer</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
                     </div>
 
-                    <div className='relative md:w-[400px] w-full font-san'>
+                    <div className=' md:w-[400px] w-full font-san'>
                         <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-4.jpg" alt="" />
-                        <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[152px] bottom-[168px]'>11 Nov, 20</p>
+                        {/* <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[152px] bottom-[168px]'>11 Nov, 20</p> */}
                         <h1 className='md:text-2xl text-xl font-semibold text-white mt-2'>How to improve work performance</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
                     </div>
 
-                    <div className='relative md:w-[400px] w-full font-san'>
+                    <div className=' md:w-[400px] w-full font-san'>
                         <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-5.jpg" alt="" />
-                        <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[152px] bottom-[144px]'>27 Dec, 19</p>
+                        {/* <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[152px] bottom-[144px]'>27 Dec, 19</p> */}
                         <h1 className='text-2xl font-semibold text-white mt-2'>How to work from home</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
                     </div>
-                    <div className='relative md:w-[400px] w-full font-san'>
-                        <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-6.jpg" alt="" />
+                    <div className=' md:w-[400px] w-full font-san'>
+                        {/* <img src="http://likeabawz.dk/exill/demo/kitzu/template/img/post-6.jpg" alt="" /> */}
                         <p className='bg-[#009e66] p-1 w-24 text-white font-semibold absolute md:bottom-[152px] bottom-[140px]'>19 Feb, 18</p>
                         <h1 className='md:text-2xl text-xl font-semibold text-white mt-2'>How to enjoy your business trip</h1>
                         <p className=' text-white mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas.</p>
